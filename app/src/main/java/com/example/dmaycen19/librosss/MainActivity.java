14127818021c51b283574868be7e049559818e55
David May <davisaac8@gmail.com>
2018-08-13 23:33:59 -0500

    segundo_commit

--- app/src/main/java/com/example/dmaycen19/librosss/MainActivity.java
+++ app/src/main/java/com/example/dmaycen19/librosss/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Esta va ser una prueba de commit, el segundo de la noche.
     }
 }
